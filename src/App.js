c6be2ed39722222b9df42854601922062aed2fb1
Najam Ul Hassan <najmulhassan721@gmail.com>
2023-04-03 19:23:48 +0500

    added the meal component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 // import logo from './logo.svg';
 import "./App.css";
 import Header from "./components/Layout/Header/Header";
+import Meal from "./components/Meals/Meal";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main>
+        <Meal />
+      </main>
     </div>
   );
 }
---
+++ src/components/Meals/Meal.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MealSummary from "./MealSummary";
+import AvailableMeals from "./AvailableMeals";
+
+function Meal() {
+  return (
+    <div>
+      <MealSummary />
+      <AvailableMeals />
+    </div>
+  );
+}
+
+export default Meal;
